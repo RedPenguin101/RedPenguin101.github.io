ccf9b08aec8d51a8781a6633ca9ab92d33eed177
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-04-06 12:26:10 +0100

    sicp sequences

--- sicp/sicp.clj
+++ sicp/sicp.clj
@@ -329,4 +329,70 @@
 (sum-odd-squares '(1 (2 3) 4 5 (6 7)))
 ;; => 84
 
+;; quick prime - presumably earlier in the book, but haven't got there
+;; uses 6k+-1 optimisation
 
+(defn prime? [n]
+  (cond (<= n 3) (> n 1)
+        (or (zero? (mod n 2)) (zero? (mod n 3))) false
+        :else (not (some (fn [i] (or (zero? (mod n i)) (zero? (mod n (+ i 2))))) (range 5 (int (Math/sqrt n)) 6)))))
+
+(map (fn [j] (list 5 j)) (enumerate-interval 1 (- 5 1)))
+;; => ((5 1) (5 2) (5 3) (5 4))
+
+
+
+(map (fn [i] (map (fn [j] (list i j)) (enumerate-interval 1 (- i 1))))
+     (enumerate-interval 1 10))
+;; => (()
+;;     ((2 1))
+;;     ((3 1) (3 2))
+;;     ((4 1) (4 2) (4 3))
+;;     ((5 1) (5 2) (5 3) (5 4))
+;;     ((6 1) (6 2) (6 3) (6 4) (6 5))
+;;     ((7 1) (7 2) (7 3) (7 4) (7 5) (7 6))
+;;     ((8 1) (8 2) (8 3) (8 4) (8 5) (8 6) (8 7))
+;;     ((9 1) (9 2) (9 3) (9 4) (9 5) (9 6) (9 7) (9 8))
+;;     ((10 1) (10 2) (10 3) (10 4) (10 5) (10 6) (10 7) (10 8) (10 9)))
+
+
+(accumulate append
+            nil
+            (map (fn [i] (map (fn [j] (list i j)) (enumerate-interval 1 (- i 1))))
+                 (enumerate-interval 1 10)))
+;; => ((2 1)
+;;     (3 1)
+;;     (3 2)
+;;     (4 1)
+;;     (4 2)
+;;     (4 3)
+;;     (5 1)
+;;     (5 2)
+;;     etc.
+
+
+(defn flatmap [f xs]
+  (accumulate append nil (map f xs)))
+
+(flatmap (fn [i] (map (fn [j] (list i j)) (enumerate-interval 1 (- i 1))))
+         (enumerate-interval 1 10))
+
+(defn prime-sum? [pair]
+  (prime? (+ (first pair) (second pair))))
+
+(defn make-pair-sum [pair]
+  (list (first pair) (second pair) (+ (first pair) (second pair))))
+
+(defn prime-sum-pairs [n]
+  (->> (enumerate-interval 1 n)
+       (flatmap (fn [i] (map (fn [j] (list i j)) (enumerate-interval 1 (- i 1)))))
+       (filter prime-sum?)
+       (map make-pair-sum)))
+
+(prime-sum-pairs 6)
+;; => ((2 1 3) (3 2 5) (4 1 5) (4 3 7) (5 2 7) (6 1 7) (6 5 11))
+
+(defn nest-map [f enum s]
+  (flatmap (fn [i] (map (fn [j] (f i j)) (enum i))) s))
+
+(nest-map list #(enumerate-interval 1 (- % 1)) (enumerate-interval 1 10))
